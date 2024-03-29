de29ceb6f5fadb1d30a6e71b02b2d2dbf06ffb60
Shaunnaa <107536959+Shaunnaa@users.noreply.github.com>
2024-03-29 23:10:58 +0700

    Update: Back-end

--- Front-end/js/navbar.js
+++ Front-end/js/navbar.js
@@ -13,23 +13,23 @@ class NavBar extends HTMLElement {
         <div class="nav-links duration-500 md:static absolute bg-[#BC3535] md:min-h-fit min-h-[60vh] left-0 top-[-100%] mid:w-auto-full flex items-center px-5"> <!--this line use for responsive-->
             <ul class="flex md:flex-row flex-col md:items-center mid:gap-[4vw] gap-12">
                 <li>
-                    <a class="text-white hover:text-gray-500" href="./Index.html">Home</a>
+                    <a class="text-white hover:text-gray-500" href="./home">Home</a>
                 </li>
                 <li class="relative">
                     <a class="text-white hover:text-gray-500" href="#">Restaurant</a>
                     <!-- Dropdown Menu -->
                     <ul class="absolute top-full left-0 bg-[#BC3535] w-32 rounded-lg shadow-md hidden">
-                        <li><a class="block px-4 py-2 text-white hover:bg-[#8B1010]" href="./TopPick.html">Top pick</a></li>
-                        <li><a class="block px-4 py-2 text-white hover:bg-[#8B1010]" href="./CloseToYou.html">Close To You</a></li>
+                        <li><a class="block px-4 py-2 text-white hover:bg-[#8B1010]" href="./TopPick">Top pick</a></li>
+                        <li><a class="block px-4 py-2 text-white hover:bg-[#8B1010]" href="./Close-to-you">Close To You</a></li>
                     </ul>
                 </li>
                 
                 
                 <li>
-                    <a class="text-white hover:text-gray-500" href="./Category.html">Category</a>
+                    <a class="text-white hover:text-gray-500" href="./Category">Category</a>
                 </li>
                 <li>
-                    <a class="text-white hover:text-gray-500" href="./About.html">About</a>
+                    <a class="text-white hover:text-gray-500" href="./About">About</a>
                 </li>
             </ul>
         </div>
@@ -44,10 +44,10 @@ class NavBar extends HTMLElement {
         <!--Search section-->
         <form class="max-w-lg mx-auto">
             <div class="flex">
-                <a id="dropdown-button" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 
+                <button id="dropdown-button" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 
                                                     border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-1 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 
                                                     dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" 
-                                             type="button" href="Adv_Search.html">Advanced Search</a> <!--focus:ring-4 = When click have a border show-->
+                                             type="button">Advanced Search</button> <!--focus:ring-4 = When click have a border show-->
 
                 <div class="relative w-full">
                     <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search " required />
@@ -74,7 +74,9 @@ class NavBar extends HTMLElement {
 }
 
 customElements.define('navbar-component', NavBar);
-
+// document.addEventListener("DOMContentLoaded", function () {
+//     customElements.define('navbar-component', NavBar);
+// });
 
 const navLinks = document.querySelector('.nav-links')
 const locationIconContainer = document.querySelector('location_icon');
@@ -87,7 +89,7 @@ function onToggleMenu(e) {
 
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var restaurantLink = document.querySelector('.relative > a'); // Get the "Restaurant" link
     var dropdownMenu = restaurantLink.nextElementSibling; // Get the dropdown menu
 
@@ -97,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Event listener to toggle the dropdown menu when the link is clicked
-    restaurantLink.addEventListener('click', function(event) {
+    restaurantLink.addEventListener('click', function (event) {
         event.preventDefault(); // Prevent the link from being followed
         toggleDropdown(); // Call the toggleDropdown function
     });
