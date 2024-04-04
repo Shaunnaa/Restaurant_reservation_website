8766b3a8eb2aef1e4a8fdefd49c6e2db558ae7da
ZethAthia <paopao.napat@gmail.com>
2024-04-04 16:58:46 +0700

    Search complete

--- Front-end/js/navbar.js
+++ Front-end/js/navbar.js
@@ -42,7 +42,7 @@ class NavBar extends HTMLElement {
 
 
         <!--Search section-->
-        <form class="max-w-lg mx-auto">
+        <form class="max-w-lg mx-auto" action="/search-summit" method="post">
             <div class="flex">
                 <button id="dropdown-button" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 
                                                     border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-1 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 
@@ -50,7 +50,7 @@ class NavBar extends HTMLElement {
                                              type="button">Advanced Search</button> <!--focus:ring-4 = When click have a border show-->
 
                 <div class="relative w-full">
-                    <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search " required />
+                    <input type="search" id="search-dropdown" name = "searchdropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search " required />
                     <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 
                                                  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 
                                                  dark:focus:ring-blue-800">
